5cc0d0af9fb538657455e8fbbf4f78909510cc22
Eamonn de Leastar <edeleastar@wit.ie>
2021-03-23 10:27:59 +0000

    first simple form

--- src/App.svelte
+++ src/App.svelte
@@ -8,4 +8,13 @@
 			<div class="uk-text-muted uk-text-small">Fun things to do</div>
 		</div>
 	</div>
+	<div class="uk-width-1-2@m uk-card uk-card-default uk-padding">
+		<fieldset class="uk-fieldset">
+			<legend class="uk-legend">Enter todo item</legend>
+			<div class="uk-margin">
+				<input class="uk-input" placeholder="Todo">
+			</div>
+		</fieldset>
+		<button class="uk-button uk-button-default">Add Todo</button>
+	</div>
 </div>
