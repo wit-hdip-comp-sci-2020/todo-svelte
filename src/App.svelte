eb98244314a52b4264f4e3c1fc8ad2814b2830ea
Eamonn de Leastar <edeleastar@wit.ie>
2021-03-23 13:37:46 +0000

    done list + delete button support

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,14 @@
 <script>
   let todoText = "";
   let todoItems = [];
+  let doneItems = [];
+
+  function uuidv4() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 
   function addTodo() {
     const todo = {
@@ -10,6 +18,15 @@
     todoItems.push(todo);
     todoItems = [...todoItems];
     todoText = "";
+  }
+
+  function deleteTodo(id) {
+    const found = todoItems.findIndex((todo) => todo.id == id);
+    const done = todoItems[found];
+    todoItems.splice(found, 1);
+    todoItems = [...todoItems];
+    doneItems.push(done);
+    doneItems = [...doneItems];
   }
 </script>
 
@@ -42,6 +59,24 @@
         <tr>
           <td> {todo.text} </td>
           <td> {todo.date} </td>
+          <button on:click={deleteTodo(todo.id)}  class="uk-button uk-button-default">Delete</button>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+  <table class="uk-table uk-table-divider" id="done-table">
+    <caption> Items Completed :</caption>
+    <thead>
+      <tr>
+        <th>Task</th>
+        <th>Date</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each doneItems as todo}
+        <tr>
+          <td> {todo.text} </td>
+          <td> {todo.date}</td>
         </tr>
       {/each}
     </tbody>
