235cc04015cc15cf74f507142ffa1fcea884897c
Eamonn de Leastar <edeleastar@wit.ie>
2021-03-23 10:22:30 +0000

    starter version - uikit + simple header

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,11 @@
 <script>
-	export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<div class="uk-container">
+	<div class="uk-flex uk-flex-center uk-flex-middle uk-margin">
+		<div class="uk-width-2-3@m uk-card uk-card-default uk-padding-small uk-text-center">
+			<div class="title"> Simple Todo List</div>
+			<div class="uk-text-muted uk-text-small">Fun things to do</div>
+		</div>
+	</div>
+</div>
